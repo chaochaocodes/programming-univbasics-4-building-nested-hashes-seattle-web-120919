40678d0f8487cb211f28f86ee58a89182e44587f
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-26 19:22:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,6 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	base_hash = {:railroads => {:pieces => 4} }
-
-
 end
 
 def monopoly_with_third_tier
@@ -19,6 +17,8 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+	base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => 0} }
+
 end
 
 def monopoly_with_fourth_tier
