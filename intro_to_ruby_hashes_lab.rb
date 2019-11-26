ac3d400a88b9c0bfcedc2be5c54571f5a1afb17c
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-26 19:26:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-	base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, c:0, d:0}, :names => {a:0, b:0, c:0, d:0} } }
+	base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, d:0}, :names => {a:0, b:0, c:0, d:0} } }
 
 end
 
